4aecba7aa49172c0a37908e05ca96cee8bc2c42d
Артем <templarka890@gmail.com>
2026-05-09 20:21:45 +0700

    трекер привычек на ООП

--- habit_tracker.py
+++ habit_tracker.py
@@ -3,8 +3,6 @@ from random import *
 from tkinter import messagebox
 import sqlite3
 
-
-
 root = Tk()
 root['bg'] = 'white'
 root.title('habits_tracker')
@@ -17,7 +15,6 @@ cursor.execute('''
     CREATE TABLE IF NOT EXISTS habits (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL,
-    create_date TEXT,
     quantity INT
     )
 ''')
@@ -31,7 +28,24 @@ cursor.execute('''
 ''')
 conn.commit()
 
-def open_del_hab():
+Label_title_txt = Label(text='Трекер привычек', font=('', 18))
+
+
+def open_list_hab():
+    win_list = Toplevel(root)
+    win_list.title('Окно для просмотра всех привычек')
+    win_list.geometry('600x500')
+
+    
+    list_habit = Listbox(win_list, width=40, font=20)
+    list_habit.place(x=100,y=1)
+    er_pole = Label(text='', font=('', 15))
+    er_pole.place(x=200, y=300)
+    
+    list_habits(list_habit, er_pole)
+
+
+def open_del_hab():         # Окно удаления привычки
     win_del=Toplevel(root)
     win_del.title('Окно для удаления приывычки')
     win_del.geometry('600x500')
@@ -40,10 +54,11 @@ def open_del_hab():
         get_ent = entry_del_hab.get()
         del_habit(get_ent, er_pole, win_del)
         
-    
-    er_pole = Label(win_del, text='', font=('', 15))
+    Label_glav_text = Label(win_del, text='Удаление привычки', font=('', 20), bg='white')
+    Label_glav_text.place(x=200,y=1)
+    er_pole = Label(win_del, text='', font=('', 15), fg= 'black')
     er_pole.place(x=1,y=380)
-    label_del_hab = Label(win_del, text='Название привычки', font=('', 15))
+    label_del_hab = Label(win_del, text='Название привычки', font=('', 15), bg='white')
     inert_btn = Button(win_del, command=tap_on_insert, font=('', 20), text='Insert')
     er_pole = Label(win_del, text='', font=('',15))
     inert_btn.place(x=450,y=420)
@@ -52,8 +67,7 @@ def open_del_hab():
     entry_del_hab.place(x=220,y=100)
     er_pole.place(x=1,y=380)
 
-
-def open_win_ap():
+def open_win_ap():      # Окно добавления привычки
     win_ap = Toplevel(root)
     win_ap.title('Окно для добавления привычки')
     er_pole = Label(win_ap,text='', font=('', 15))
@@ -62,11 +76,11 @@ def open_win_ap():
         quantity = entry_quantity.get()
         name = entry_name.get()
         if name and quantity:
-            append_habit(name, er_pole, quantity, win_ap)
+            append_habit(name, er_pole,quantity, win_ap)
         else:
             er_pole.configure(text='Поля не должны быть пустыми!')
     
-    label_glav_txt = Label(win_ap, text='Добавление привчки', font=('', 15), bg='white',)
+    label_glav_txt = Label(win_ap, text='Добавление привычки', font=('', 15), bg='white')
     label_name_text = Label(win_ap, text='Название', font=('', 15), bg='white')
     label_quantity_text = Label(win_ap,text='Количество повторений', font=('', 15), bg='white')
     insert_btn = Button(win_ap, text='Insert', font=('', 20), command=tap_on_insert)
@@ -83,14 +97,14 @@ def open_win_ap():
 def append_habit(name, er_pole, quantity, win_ap):                      # func добавления привычки
     cursor.execute('''SELECT name FROM habits WHERE name = ?''', (name,))
     if cursor.fetchone():
-        er_pole.configure(text=('Такая привычка уже существует'))
+        er_pole.configure(text=('Такая привычка уже существует'), fg='red')
         return
     cursor.execute('''
     INSERT INTO habits (name, quantity)
     VALUES (?, ?)
     ''', (name, quantity))
     conn.commit()
-    er_pole.configure(text=(f'Привчка {name} {quantity} раз успешно добавлена.'), font=('',13))
+    er_pole.configure(text=f'Привычка {name} {quantity} раз успешно добавлена.', fg='green', font=('',13))
     win_ap.after(1500, win_ap.destroy)    
 
 def del_habit(name,er_pole, win_del):                        # func удаление привычки
@@ -99,26 +113,24 @@ def del_habit(name,er_pole, win_del):                        # func удален
         cursor.execute('''
         DELETE FROM habits WHERE name = ?
         ''', (name,))
-        er_pole.configure(text=f'Привычка {name} успешно удалена.')
+        er_pole.configure(text=f'Привычка {name} успешно удалена.', fg= 'green')
         win_del.after(1500, win_del.destroy)
         conn.commit()
     else:
-        er_pole.configure(text='Такая привычка не найдена')
+        er_pole.configure(text=f'Привычка {name} не найдена', fg='red')
         return
 
 
-def list_habits():                      # Список всех привычек
+def list_habits(list_habit, er_pole):                      # Список всех привычек
     cursor.execute('''
-    SELECT id, name, create_date FROM habits ''')
+    SELECT id, name, quantity FROM habits ''')
     habits = cursor.fetchall()
     if not habits:
-        print('Привычек нету, добавь первую')
+        er_pole.configure(text='Привычек пока нет!')
         return
     else:
-        for habit_id, name, date in habits:
-            print(f'ID: {habit_id}'),
-            print(f'Имя: {name}'),
-            print(f'Дата: {date}')
+        for habit_id, name, quantity in habits:
+            list_habit.insert(END, f'{habit_id}. {name} {quantity} раз.')
 
 # интерфейс в главном окне
 append_habit_btn = Button(
@@ -126,13 +138,28 @@ append_habit_btn = Button(
     command=open_win_ap,
     font=('', 20)
 )
-append_habit_btn.place(x=0,y=1)
+append_habit_btn.place(x=200,y=100)
 
 del_habit_btn= Button(
     text='Удалить привычку',
     command=open_del_hab,
     font=('',20)
 )
-del_habit_btn.place(x=1, y=444)
+del_habit_btn.place(x=210, y=300)
+
+list_habit_btn= Button(
+    text='Список всех привычек',
+    font=('', 18),
+    command=open_list_hab
+)
+list_habit_btn.place(x=200,y=200)
+
+
+
+Label_title_txt.place(x=230, y=1)
+
+
+
+
 
 root.mainloop() # Работа программы
---
+++ habit_tracker_OOP.py
@@ -0,0 +1,153 @@
+from tkinter import *
+from random import *
+from tkinter import messagebox
+import sqlite3
+
+conn = sqlite3.connect('habits_tracker.db')
+cursor = conn.cursor()
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS habits (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    quantity INT
+    )
+''')
+conn.commit() 
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS completions (
+    habit_id INTEGER
+    )
+''')
+conn.commit()
+
+class Habit_tracker:
+    def __init__(self):
+        self.root = Tk()
+        self.root.geometry('700x500')
+        self.root.resizable(height=False, width=False)
+        self.root.title('Трекер привычек')
+        
+        append_habit_btn = Button(
+            text='Добавить привычку',
+            command=self.open_win_ap,
+            font=('', 20)
+        ).place(x=200,y=100)
+    
+        del_habit_btn = Button(
+            text='Удалить привычку',
+            command=self.open_win_del_habit,
+            font=('', 20)
+        ).place(x=200,y=200)
+    
+    
+    def append_habit(self, name, quantity):
+        cursor.execute('''SELECT name FROM habits WHERE name = ?''', (name,))
+        if cursor.fetchone():
+            return False, 'Такая привычка уже существует'
+        cursor.execute('''
+        INSERT INTO habits (name, quantity)
+        VALUES (?, ?)
+        ''', (name, quantity))
+        conn.commit()
+        return True, f'Привычка {name} {quantity} раз была добавлена.'
+        
+    
+    def del_habit(self, habit_id):                        # func удаление привычки   
+        cursor.execute('''SELECT name, id FROM habits WHERE id = ?''', (habit_id,))
+        habit = cursor.fetchone()
+        if habit:        
+            name = habit[0]
+            cursor.execute('''
+            DELETE FROM habits WHERE id = ?
+            ''', (habit_id,))
+            conn.commit()
+            return True, f'Привычка {name} была удалена.'   
+        else:
+            conn.commit()
+            return False, f'Привычки c id: {habit_id} не было найдено.'
+    
+    def list_habits(self):
+        cursor.execute('''
+    SELECT id, name, quantity FROM habits ''')
+        habits = cursor.fetchall()
+        return habits
+    
+    def open_win_ap(self):      # Окно добавления привычки
+        win_ap = Toplevel(self.root)
+        win_ap.geometry('600x500')
+        win_ap.title('Окно для добавления привычки')
+        win_ap.resizable(width=False, height=False)
+        er_pole = Label(win_ap,text='', font=('', 15))
+        def tap_on_insert():
+            name = entry_name.get()
+            quantity = entry_quantity.get()
+            if name and quantity:
+                setting, message = self.append_habit(name, quantity)
+                if setting:
+                    er_pole.configure(text=message, fg='green')
+                    win_ap.after(2000, win_ap.destroy)
+                else:
+                    er_pole.configure(text=message, fg='red')
+            else:
+                er_pole.configure(text='Поля не должны быть пустыми', fg='red')
+        
+        er_pole.place(x=1,y=380)
+        label_glav_txt = Label(win_ap, text='Добавление привычки', font=('', 15), bg='white')
+        label_glav_txt.place(x=180,y=1)
+        label_name_text = Label(win_ap, text='Название', font=('', 15), bg='white')    
+        label_name_text.place(x=100,y=50)
+        label_quantity_text = Label(win_ap,text='Количество повторений', font=('', 15), bg='white')
+        label_quantity_text.place(x=25,y=200)
+        insert_btn = Button(win_ap, text='Insert', font=('', 20), command=tap_on_insert)
+        insert_btn.place(x=450,y=420)
+        entry_quantity = Entry(win_ap,font=('', 17))
+        entry_quantity.place(x=250, y= 200)
+        entry_name = Entry(win_ap, font=('', 17))
+        entry_name.place(x=250,y=50)
+
+    def open_win_del_habit(self):
+        win_del = Toplevel(self.root)
+        win_del.title('Окно для удаления привычки')
+        win_del.resizable(width=False, height=False)
+        win_del.geometry('600x500')
+        er_pole = Label(win_del, text='', font=('',15))
+        
+        def tap_on_insert():
+            ent_habit_id = entry_habit_id.get()
+            if not ent_habit_id:
+                er_pole.configure(text='Поле не должно быть пустым', fg='red')
+                return
+            setting, message = self.del_habit(ent_habit_id)
+            if setting:
+                er_pole.configure(fg='green', text=message)
+                win_del.after(2000, win_del.destroy)
+            else:
+                er_pole.configure(text=message)
+            
+                
+        insert_btn = Button(win_del, text='Удалить привычку', command=tap_on_insert, font=('', 15))
+        insert_btn.place(x=400,y=420)
+        er_pole.place(x=1,y=380)
+        entry_habit_id = Entry(win_del, font=('', 17))
+        entry_habit_id.place(x=250,y=100)
+        label_habit_id_text = Label(win_del, text='Введите id привычки', font=('', 17))
+        label_habit_id_text.place(x=1,y=100)
+
+        
+
+
+
+
+        
+    def run(self):
+        self.root.mainloop()
+
+
+
+
+
+main_win = Habit_tracker()
+main_win.run()
+
+
